648e4ec4934322fbbf19a3be2a6a6b8262e736ad
karl.mbp <chinazdg@163.cm>
2020-11-16 15:27:37 +0800

    add locales

---
+++ README.md
@@ -0,0 +1,12 @@
+# vue-cli-plugin-fleetup
+
+## Feature
+- Add Task
+
+## Changelogs
+
+### v1.0.0
+2020-11-16
+
+- add task "gulp", "s3 upload"
+- i18n support: US_en, ZH_cn
--- ui.js
+++ ui.js
@@ -4,28 +4,25 @@ module.exports = api => {
   api.describeConfig({
     id: 'vue-cli-plugin-fleetup',
     name: 'FleetUp vue-cli-plugin',
-    description: 'FleetUp Customer Task for Vue.js',
+    description: 'org.vue.common.description',
     link: 'https://github.com/chinazdg/vue-cli-plugin-fleetup'
   })
 
   api.addClientAddon({
     id: 'org.vue.webpack.client-addon',
-    // 包含构建出来的 JS 文件的文件夹
     path: '@vue/cli-ui-addon-webpack/dist'
   })
   // 在这里使用 API...
   api.describeTask({
-    // 用于匹配脚本命令的 RegExp 对象，来选择要被描述的任务
     match: /gulp/,
-    description: '前置任务预执行，elementUi、国际化',
+    description: 'org.vue.tasks.pre-task',
     // “More info”链接
     link: 'https://www.gulpjs.com.cn/',
     icon: '/_plugin/vue-cli-plugin-fleetup/gulp.png'
   })
   api.describeTask({
-    // 用于匹配脚本命令的 RegExp 对象，来选择要被描述的任务
     match: /aws s3 sync .\/dist\//,
-    description: '部署到AWS S3',
+    description: 'org.vue.tasks.s3-upload',
     link: 'https://s3.console.aws.amazon.com/',
     icon: '/_plugin/vue-cli-plugin-fleetup/aws-s3.png'
   })
